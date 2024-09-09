3aea9ab1a4c0ec753017320a890f5dcf055a63a3
rxdron <rxdron@gmail.com>
2024-09-10 01:33:45 +0500

    add Jenkins in tests

--- src/test/java/example/Allure/TestBaseExtend.java
+++ src/test/java/example/Allure/TestBaseExtend.java
@@ -4,11 +4,13 @@ package example.Allure;
 import Helper.Attach;
 import application.RegistrationStudy;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import randomData.TestData;
 
@@ -27,13 +29,16 @@ public class TestBaseExtend {
         ));
 
         Configuration.browserCapabilities = capabilities;
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
     }
 
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
         Configuration.browser = "chrome";
         Configuration.browserVersion = "126.0";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
